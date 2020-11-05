803611f83af69aeee21d1a0c7d5659389a0bba96
boybeak <boybeak@gmail.com>
2020-11-06 02:29:45 +0800

    update

--- app/src/main/java/com/github/boybeak/gifhelper/adapter/GifHolder.kt
+++ app/src/main/java/com/github/boybeak/gifhelper/adapter/GifHolder.kt
@@ -1,15 +1,22 @@
-package com.github.boybeak.gifhelper
+package com.github.boybeak.gifhelper.adapter
 
-import android.support.rastermill.FrameSequenceDrawable
 import android.view.View
 import android.widget.ImageView
-import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.bumptech.glide.request.RequestOptions
 import com.github.boybeak.adapter.AbsHolder
 import com.github.boybeak.adapter.AnyAdapter
+import com.github.boybeak.gifhelper.R
+import com.github.boybeak.gifhelper.gif.GlideApp
 
 class GifHolder(v: View) : AbsHolder<GifItem>(v) {
+
+    companion object {
+        private val BG_COLORS = arrayOf(
+            android.R.color.holo_blue_bright, android.R.color.holo_green_dark,
+            android.R.color.holo_orange_dark, android.R.color.holo_purple,
+            android.R.color.holo_red_dark)
+    }
+
     private val gifIV = view<ImageView>(R.id.gifIV)
     override fun onBind(item: GifItem, position: Int, absAdapter: AnyAdapter) {
         if (item.useGifX) {
@@ -17,6 +24,7 @@ class GifHolder(v: View) : AbsHolder<GifItem>(v) {
         } else {
             Glide.with(itemView).load(item.source()).into(gifIV)
         }
+        itemView.setBackgroundColor(context().getColor(BG_COLORS[position % BG_COLORS.size]))
     }
 
 }
